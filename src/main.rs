3a827bd030db9ab8d77e0eeae2d2b7ad12267082
Reto Hablützel <rethab@protonmail.ch>
2021-03-17 20:07:01 +0100

    dns lookups

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ extern crate structopt;
 
 use std::collections::HashMap;
 use std::error::Error;
+use std::io;
 use std::net::SocketAddr;
+use std::str::FromStr;
 use std::time::{Duration, Instant};
 
 use structopt::StructOpt;
@@ -28,7 +30,23 @@ struct Opts {
     #[structopt(short, long)]
     timelimit: Option<u64>,
 
-    url: Url,
+    url: LenientUrl,
+}
+
+#[derive(Debug)]
+struct LenientUrl(Url);
+
+impl FromStr for LenientUrl {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let url: String = if !s.starts_with("http://") && !s.starts_with("https://") {
+            format!("http://{}", s)
+        } else {
+            s.to_owned()
+        };
+        Url::parse(&url).map_err(|_| format!("invalid URL")).map(LenientUrl)
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -44,9 +62,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let timelimit = Duration::from_secs(opt.timelimit.unwrap_or(u64::max_value()));
 
-    let addr: SocketAddr = create_socket_addr(&opt.url);
+    let addr: SocketAddr = create_socket_addr(&opt.url.0)?;
 
-    let req = create_request(&opt.url);
+    let req = create_request(&opt.url.0);
     let request = req.as_bytes();
 
     let mut ctx = Ctx::new(request, opt.requests)?;
@@ -67,14 +85,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn create_socket_addr(url: &Url) -> SocketAddr {
-    let port = url
-        .port()
-        .unwrap_or_else(|| if url.scheme() == "https" { 443 } else { 80 });
-    let host = url.host_str().expect("Missing host");
-    let addr = format!("{}:{}", host, port);
-    addr.parse()
-        .unwrap_or_else(|_| panic!("Failed to parse {} as SocketAddr", addr))
+fn create_socket_addr(url: &Url) -> io::Result<SocketAddr> {
+    url.socket_addrs(|| url.port_or_known_default()).map(|ss| ss[0])
 }
 
 fn create_request(url: &Url) -> String {
@@ -84,4 +96,32 @@ fn create_request(url: &Url) -> String {
         "GET {} HTTP/1.0\r\nHost: {}\r\n{}\r\n\r\n",
         path, host, "Accept: */*"
     )
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_create_socket_addr() {
+        let localhost_port: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+        let localhost_http: SocketAddr = "127.0.0.1:80".parse().unwrap();
+        let localhost_https: SocketAddr = "127.0.0.1:443".parse().unwrap();
+        assert_eq!(localhost_port, create_socket_addr(&parse_url("localhost:8080").0).unwrap());
+        assert_eq!(localhost_port, create_socket_addr(&parse_url("http://localhost:8080").0).unwrap());
+        assert_eq!(localhost_http, create_socket_addr(&parse_url("http://localhost").0).unwrap());
+        assert_eq!(localhost_http, create_socket_addr(&parse_url("http://localhost:80").0).unwrap());
+        assert_eq!(localhost_https, create_socket_addr(&parse_url("https://localhost").0).unwrap());
+        assert_eq!(localhost_https, create_socket_addr(&parse_url("https://localhost:443").0).unwrap());
+    }
+
+    #[test]
+    fn test_lenient_url_from_str() {
+        assert_eq!("http", LenientUrl::from_str("localhost").unwrap().0.scheme());
+        assert_eq!("localhost", LenientUrl::from_str("localhost").unwrap().0.host_str().unwrap());
+    }
+
+    fn parse_url(url: &str) -> LenientUrl {
+        LenientUrl::from_str(url).unwrap()
+    }
 }
