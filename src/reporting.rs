8f53ac5dff27e404caae9dba4e3deef1602a1611
Reto Hablützel <rethab@protonmail.ch>
2021-03-19 20:49:21 +0100

    report time to connect, #8

--- src/reporting.rs
+++ src/reporting.rs
@@ -15,12 +15,13 @@ pub struct Reporter {
 struct ConnectionStats {
     state: State,
     times: Vec<Duration>,
+    ctimes: Vec<Duration>, // connection times
 }
 
 #[derive(Debug)]
 enum State {
     Unconnected,
-    Connecting,
+    Connecting(Instant),
     Connected,
     Read(Instant),
 }
@@ -55,11 +56,12 @@ impl Reporter {
                 stats.times.push(Instant::now() - *started);
                 stats.state = Unconnected;
             }
-            (_, CONNECTED) => {
+            (Connecting(started), CONNECTED) => {
+                stats.ctimes.push(Instant::now() - *started);
                 stats.state = Connected;
             }
             (_, CONNECTING) => {
-                stats.state = Connecting;
+                stats.state = Connecting(Instant::now());
             }
             invalid => panic!(
                 "Invalid state transition from {:?} to {:?}",
@@ -75,6 +77,7 @@ impl Reporter {
                 ConnectionStats {
                     state: State::Unconnected,
                     times: vec![],
+                    ctimes: vec![],
                 },
             );
         }
@@ -107,7 +110,37 @@ impl Reporter {
         println!("Non-2xx responses:\t{}", ctx.unsuccessful_responses);
 
         println!();
+        self.print_connection_times();
+        println!();
+        self.print_response_times();
+    }
 
+    fn print_connection_times(&self) {
+        let mut ctimes: Vec<Duration> = self
+            .connections
+            .iter()
+            .flat_map(|(_, c)| c.ctimes.clone())
+            .collect();
+
+        if ctimes.is_empty() {
+            return;
+        }
+
+        ctimes.sort_unstable();
+
+        println!("Connection Times (ms)");
+        println!("\t\tmin  mean[+/-sd] median   max");
+        println!(
+            "Connect:\t{: >3}{: >5.0}{: >6.1}{: >5}{: >10}",
+            min(&ctimes),
+            mean(&ctimes),
+            std_dev(&ctimes),
+            median(&ctimes),
+            max(&ctimes)
+        );
+    }
+
+    fn print_response_times(&self) {
         let mut all_times: Vec<Duration> = self
             .connections
             .iter()
@@ -126,5 +159,80 @@ impl Reporter {
                 println!("100%\t{} (longest request)", longest.as_millis());
             }
         }
+    }
+}
+
+fn min(times: &[Duration]) -> u128 {
+    times.first().unwrap().as_millis()
+}
+
+fn mean(times: &[Duration]) -> f32 {
+    let sum: Duration = times.iter().sum();
+    sum.as_millis() as f32 / times.len() as f32
+}
+
+fn std_dev(times: &[Duration]) -> f32 {
+    let mean = mean(times);
+    let variance = times
+        .iter()
+        .map(|t| (t.as_millis() as f32 - mean).powf(2.0))
+        .sum::<f32>()
+        / (times.len() - 1) as f32;
+    variance.sqrt()
+}
+
+fn median(times: &[Duration]) -> u128 {
+    times[times.len() / 2].as_millis()
+}
+
+fn max(times: &[Duration]) -> u128 {
+    times.last().unwrap().as_millis()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_min() {
+        assert_eq!(1, min(&[d(1), d(2), d(3)]));
+        assert_eq!(2, min(&[d(2), d(3)]));
+    }
+
+    #[test]
+    fn test_mean() {
+        assert_eq!(2.0, mean(&[d(1), d(2), d(3)]));
+        assert_eq!(2.5, mean(&[d(2), d(3)]));
+        assert_eq!(5.0, mean(&[d(4), d(5), d(6)]));
+        assert_eq!(6.0, mean(&[d(1), d(1), d(1), d(3), d(24)]));
+        assert_eq!(5.5, mean(&[d(5), d(6)]));
+    }
+
+    #[test]
+    fn test_std_dev() {
+        assert_eq!(1.9148543, std_dev(&[d(3), d(5), d(7), d(7)]));
+        assert_eq!(
+            2.13809,
+            std_dev(&[d(2), d(4), d(4), d(4), d(5), d(5), d(7), d(9)])
+        );
+    }
+
+    #[test]
+    fn test_median() {
+        assert_eq!(1, median(&[d(1)]));
+        assert_eq!(2, median(&[d(1), d(2)]));
+        assert_eq!(2, median(&[d(1), d(2), d(3)]));
+        assert_eq!(5, median(&[d(4), d(5), d(6)]));
+        assert_eq!(1, median(&[d(1), d(1), d(1), d(3), d(24)]));
+    }
+
+    #[test]
+    fn test_max() {
+        assert_eq!(3, max(&[d(1), d(2), d(3)]));
+        assert_eq!(3, max(&[d(2), d(3)]));
+    }
+
+    fn d(ms: u64) -> Duration {
+        Duration::from_millis(ms)
     }
 }
